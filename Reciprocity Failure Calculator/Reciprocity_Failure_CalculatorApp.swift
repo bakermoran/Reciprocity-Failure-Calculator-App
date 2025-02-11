5aa899db5d5e7bec06263ee85ca9e825b6ea7774
Baker Moran <baker.moran@nexhealth.com>
2025-02-10 18:05:03 -0800

    Initial Commit

---
+++ Reciprocity Failure Calculator/Reciprocity_Failure_CalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  Reciprocity_Failure_CalculatorApp.swift
+//  Reciprocity Failure Calculator
+//
+//  Created by Baker Moran on 2/10/25.
+//
+
+import SwiftUI
+
+@main
+struct Reciprocity_Failure_CalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Reciprocity Failure CalculatorTests/Reciprocity_Failure_CalculatorTests.swift
@@ -0,0 +1,17 @@
+//
+//  Reciprocity_Failure_CalculatorTests.swift
+//  Reciprocity Failure CalculatorTests
+//
+//  Created by Baker Moran on 2/10/25.
+//
+
+import Testing
+@testable import Reciprocity_Failure_Calculator
+
+struct Reciprocity_Failure_CalculatorTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
